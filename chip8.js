43547f8af60b4c7df3831af0df2c5970bea7c22a
thien pham <47543920+ThienTheCreator@users.noreply.github.com>
2023-09-11 22:58:58 -0700

    added sprites into memory

--- chip8.js
+++ chip8.js
@@ -58,4 +58,102 @@ async function time() {
 	}
 }
 
-time();
+function spriteSetup(){
+	memory[0]  = 0b11110000;
+	memory[1]  = 0b10010000;
+	memory[2]  = 0b10010000;
+	memory[3]  = 0b10010000;
+	memory[4]  = 0b11110000;
+	
+	memory[5]  = 0b00100000;
+	memory[6]  = 0b01100000;
+	memory[7]  = 0b00100000;
+	memory[8]  = 0b00100000;
+	memory[9]  = 0b01110000;
+
+	memory[10] = 0b11110000;
+	memory[11] = 0b00010000;
+	memory[12] = 0b11110000;
+	memory[13] = 0b10000000;
+	memory[14] = 0b11110000; 
+	
+	memory[15] = 0b11110000;
+	memory[16] = 0b00010000;
+	memory[17] = 0b11110000;
+	memory[18] = 0b00010000;
+	memory[19] = 0b11110000;
+	
+	memory[20] = 0b10010000;
+	memory[21] = 0b10010000;
+	memory[22] = 0b11110000;
+	memory[23] = 0b00010000;
+	memory[24] = 0b00010000;
+
+	memory[25] = 0b11110000;
+	memory[26] = 0b10000000;
+	memory[27] = 0b11110000;
+	memory[28] = 0b00010000;
+	memory[29] = 0b11110000;
+
+	memory[30] = 0b11110000;
+	memory[31] = 0b10000000;
+	memory[32] = 0b11110000;
+	memory[33] = 0b10010000;
+	memory[34] = 0b11110000;
+	
+	memory[35] = 0b11110000;
+	memory[36] = 0b00010000;
+	memory[37] = 0b00100000;
+	memory[38] = 0b01000000;
+	memory[39] = 0b01000000;
+
+	memory[40] = 0b11110000;
+	memory[41] = 0b10010000;
+	memory[42] = 0b11110000;
+	memory[43] = 0b10010000;
+	memory[44] = 0b11110000;
+	
+	memory[45] = 0b11110000;
+	memory[46] = 0b10010000;
+	memory[47] = 0b11110000;
+	memory[48] = 0b00010000;
+	memory[49] = 0b11110000;
+
+	memory[50] = 0b11110000;
+	memory[51] = 0b10010000;
+	memory[52] = 0b11110000;
+	memory[53] = 0b10010000;
+	memory[54] = 0b10010000;
+	
+	memory[55] = 0b11100000;
+	memory[56] = 0b10010000;
+	memory[57] = 0b11100000;
+	memory[58] = 0b10010000;
+	memory[59] = 0b11100000;
+
+	memory[60] = 0b11110000;
+	memory[61] = 0b10000000;
+	memory[62] = 0b10000000;
+	memory[63] = 0b10000000;
+	memory[64] = 0b11110000;
+
+	memory[65] = 0b11100000;
+	memory[66] = 0b10010000;
+	memory[67] = 0b10010000;
+	memory[68] = 0b10010000;
+	memory[69] = 0b11100000;
+
+	memory[70] = 0b11110000;
+	memory[71] = 0b10000000;
+	memory[72] = 0b11110000;
+	memory[73] = 0b10000000;
+	memory[74] = 0b11110000;
+
+	memory[75] = 0b11110000;
+	memory[76] = 0b10000000;
+	memory[77] = 0b11110000;
+	memory[78] = 0b10000000;
+	memory[79] = 0b10000000;
+}
+spriteSetup();
+
